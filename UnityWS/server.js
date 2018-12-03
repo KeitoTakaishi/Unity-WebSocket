59b6dca167156b0a55b969798a640dc4bb4574e9
takaishi keito <keitoncis@icloud.com>
2018-12-04 00:31:16 +0900

    update UnityWS

--- UnityWS/server.js
+++ UnityWS/server.js
@@ -10,31 +10,42 @@ const hostname = '127.0.0.1';
 const port = 3000;
  
 var server = http.createServer(getFromClient);
-
+var temp = '';
+var num = 0;
 function getFromClient(req, res){
     //trueをつけることでqueryがきても対応できるようにする
     var rute = url.parse(req.url, true);
     switch(rute.pathname){
         case '/':
-            var body = '';
+        num = 0;
             if(req.method == 'POST' || req.method == 'GET'){
-                console.log('-- case / --');
+                console.log('-- case / --' + 'num = ' + num );
                 var query = rute.query;
-                if(query.msg != undefined){
-                    console.log(query.msg);
-                    wss.on('connection', function(ws) {
-                        console.log('WS-Connect-Suc!');
-                        ws.send('recieve');
-                        ws.send(query.msg);
-                    });
+                var msg = query.msg;
+                if(msg != undefined){
+                    
+                    //wssははじめにコネクトしたリクエストに依存してコネクトされる
+                     if(temp !== msg){
+                         console.log("query"+msg);
+                         wss.on('connection', function(ws) {
+                             if(num === 0){
+                                 //console.log('WS-Connect-Suc!');
+                                 ws.send(temp);
+                                 console.log('temp:  ' + temp);
+                                 console.log('query.msg:  ' + msg); 
+                                 num = 1;
+                             }
+                         });
+                         temp = msg;
+                     }else{
+                         console.log('same');
+                     }
                 }
 
                 req.on('data', function(chunk){
-                    body += chunk;
+                    //body += chunk;
                 });
                 req.on('end', function(){
-                    
-                    
                     res.end();
                 });
             }
