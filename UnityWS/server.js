a9bb683535fcada88e59d3d02a6a1f2372e391fd
takaishi keito <keitoncis@icloud.com>
2018-12-04 11:25:17 +0900

    able to observe ColorCode on HTML

--- UnityWS/server.js
+++ UnityWS/server.js
@@ -6,56 +6,60 @@ var wss = new WebSocketServer({
 //-----------------------------
 const http = require('http');
 const url = require('url');
-const hostname = '127.0.0.1';
+const ejs = require('ejs');
+const fs = require('fs');
 const port = 3000;
- 
+
+const index_page = fs.readFileSync('./index.ejs', 'utf8');
+const style_css = fs.readFileSync('./style.css', 'utf8');
+
 var server = http.createServer(getFromClient);
-var temp = '';
-var num = 0;
+var temp = '';//for ws sending
+var isAccept = true;
+//var rute;
+
 function getFromClient(req, res){
     //trueをつけることでqueryがきても対応できるようにする
-    var rute = url.parse(req.url, true);
-    switch(rute.pathname){
+    var route = url.parse(req.url, true);
+    switch(route.pathname){
         case '/':
-        num = 0;
-            if(req.method == 'POST' || req.method == 'GET'){
-                console.log('-- case / --' + 'num = ' + num );
-                var query = rute.query;
-                var msg = query.msg;
-                if(msg != undefined){
-                    
-                    //wssははじめにコネクトしたリクエストに依存してコネクトされる
-                     if(temp !== msg){
-                         console.log("query"+msg);
-                         wss.on('connection', function(ws) {
-                             if(num === 0){
-                                 //console.log('WS-Connect-Suc!');
-                                 ws.send(temp);
-                                 console.log('temp:  ' + temp);
-                                 console.log('query.msg:  ' + msg); 
-                                 num = 1;
-                             }
-                         });
-                         temp = msg;
-                     }else{
-                         console.log('same');
-                     }
-                }
-
-                req.on('data', function(chunk){
-                    //body += chunk;
-                });
-                req.on('end', function(){
-                    res.end();
-                });
-            }
-		break;
+            queryChecker(req, res, route);
+            var content = ejs.render(index_page, {
+                content: temp,
+            });
+            res.writeHead(200, {'Content-Type': 'text/html'});
+          	res.write(content);
+          	res.end();
+		        break;
 	}
-
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write('suc!-from Node.js');
-	res.end();
 }
 
 server.listen(3000);
 console.log('start server');
+
+function queryChecker(req, res, route){
+  if(req.method == 'POST' || req.method == 'GET'){
+      var query = route.query;
+      if(query.msg != undefined){
+        isAccept = true;
+         if(temp !== query.msg){
+             console.log("value : "+query.msg);
+             wss.on('connection', function(ws) {
+                 if(isAccept === true){
+                     isAccept = false;
+                     ws.send(temp);
+                     console.log('value:  ' + temp);
+                     // var r,g,b;
+                     // var r = query.msg.split(',')[0];
+                     // var g = query.msg.split(',')[1];
+                     // var b = query.msg.split(',')[2];
+                     // console.log('query.msg:  ' + r + g + b);
+                 }
+             });
+             temp = query.msg;
+         }else{
+             console.log('value : same');
+         }
+       }
+  }
+}
